5a0fd6a5bd399bc3886babe5e40b8cd21272b348
Mauricio Landa <landamauricio1@gmail.com>
2020-06-29 17:10:03 -0400

    fix: expand verhoeff return value

--- src/codigo-de-control/steps/step-1/index.js
+++ src/codigo-de-control/steps/step-1/index.js
@@ -1,11 +1,10 @@
-/* eslint-disable no-unused-vars */
 const { verhoeff } = require('../../../crypto/verhoeff/index')
 
-const addRecursive = (number, digits) => {
+const appendVerhoeff = (number, amount) => {
   let temp = number
+  let digits = amount
   while (digits > 0) {
     temp += verhoeff(temp)
-    // eslint-disable-next-line no-param-reassign
     digits -= 1
   }
   return temp;
@@ -16,19 +15,31 @@ const stepOne = (data) => {
     number = null, nitci = null, date = null, amount = null,
   } = data
 
-  if (!number || !nitci || !date || !amount) { throw TypeError('Missing parameters') }
-  const number2verhoeff = addRecursive(number, 2)
-  const nitci2verhoeff = addRecursive(nitci, 2)
-  const date2verhoeff = addRecursive(date, 2)
-  const amount2verhoeff = addRecursive(amount, 2)
+  if (!number || !nitci || !date || !amount) {
+    throw TypeError('number, nitci, date and amount are required for data in step 1')
+  }
+  const numberVerhoeff = appendVerhoeff(number, 2)
+  const nitciVerhoeff = appendVerhoeff(nitci, 2)
+  const dateVerhoeff = appendVerhoeff(date, 2)
+  const amountVerhoeff = appendVerhoeff(amount, 2)
 
-  const totalSum = parseInt(number, 10)
-  + parseInt(nitci, 10)
-  + parseInt(date, 10)
-  + parseInt(amount, 10)
+  const dataTotal = (
+    parseInt(number, 10)
+    + parseInt(nitci, 10)
+    + parseInt(date, 10)
+    + parseInt(amount, 10)
+  )
+  const totalSumString = dataTotal.toString()
 
-  const totalSumString = totalSum.toString()
-  return addRecursive(totalSumString, 5).slice(-5)
+  return {
+    verhoeff5: appendVerhoeff(totalSumString, 5).slice(-5),
+    verhoeff2: {
+      number: numberVerhoeff,
+      nitci: nitciVerhoeff,
+      date: dateVerhoeff,
+      amount: amountVerhoeff,
+    },
+  }
 }
 
 module.exports = {
