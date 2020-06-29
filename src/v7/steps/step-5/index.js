d84747c23ab35e371b60a122ed9565fe49fb89bb
Mauricio Landa <landamauricio1@gmail.com>
2020-06-29 18:55:46 -0400

    fix: add step 5

---
+++ src/v7/steps/step-5/index.js
@@ -0,0 +1,26 @@
+const { base64 } = require('../../../crypto/base-64')
+
+const stepFive = (data = {}) => {
+  const {
+    verhoeff5, verhoeff, total, partialTotals,
+  } = data
+
+  if (!verhoeff5 || !verhoeff || !total || !partialTotals) {
+    throw TypeError('data is required for step 5')
+  }
+
+  const result = partialTotals.map((partial, idx) => parseInt((partial * total) / verhoeff[idx], 10))
+
+  const message = result.reduce((a, b) => a + b, 0)
+
+  const encoded = base64(message)
+
+  return {
+    verhoeff5,
+    encoded,
+  }
+}
+
+module.exports = {
+  stepFive,
+}
